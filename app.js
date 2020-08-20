d5c0258756fb075067cbd35fe3b611dab4144575
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-08-20 18:07:56 +0700

    + hbs helpers

--- app.js
+++ app.js
@@ -32,8 +32,14 @@ if (process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'));
 }
 
+//handlebars helpers
+const { formatDate } = require('./helpers/hbs');
+
 // handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine(
+    '.hbs',
+    exphbs({ helpers: { formatDate }, defaultLayout: 'main', extname: '.hbs' })
+);
 app.set('view engine', '.hbs');
 
 // express session middleware
