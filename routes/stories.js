2a067126865b6034fd4e73698d092bcae5b035ee
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-08-21 05:55:47 +0700

    + show detail stories

--- routes/stories.js
+++ routes/stories.js
@@ -20,6 +20,21 @@ router.get('/', ensureAuth, async (req, res) => {
     }
 });
 
+router.get('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id).populate('user').lean();
+
+        if (!story) {
+            return res.render('error/404');
+        }
+
+        res.render('stories/show', { story });
+    } catch (error) {
+        console.error(error);
+        res.render('error/404');
+    }
+});
+
 router.post('/', ensureAuth, async (req, res) => {
     try {
         req.body.user = req.user.id;
@@ -35,7 +50,7 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
     try {
         const story = await Story.findOne({ _id: req.params.id }).lean();
     } catch (error) {
-        res.render('error/500'); 
+        res.render('error/500');
     }
     if (!story) {
         return res.render('error/404');
@@ -77,6 +92,21 @@ router.delete('/:id', ensureAuth, async (req, res) => {
     try {
         await Story.remove({ _id: req.params.id });
         res.redirect('/dashboard');
+    } catch (error) {
+        console.error(error);
+        return res.render('error/500');
+    }
+});
+
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public'
+        })
+            .populate('user')
+            .lean();
+        res.render('stories/index', { stories });
     } catch (error) {
         console.error(error);
         return res.render('error/500');
