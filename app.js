dbe929b8d3abbb993d33bbe4483a4bdf5a090c40
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-08-20 10:35:39 +0700

    + view layouts

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars');
+const connectDB = require('./config/db');
+const indexRoutes = require('./routes/index')
 
-//Load config
+//load config
 dotenv.config({ path: './config/config.env' });
 
+//connect db
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+//logging
+if(process.env.NODE_ENV == 'development') {
+    app.use(morgan('dev'))
+}
+
+// handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main' ,extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//routes
+app.use('/', indexRoutes);
+
+//listen 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
     console.log(
         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
