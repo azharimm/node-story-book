03d09dedf96563ac9b81d7162c50d098454f59d1
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-08-20 19:31:38 +0700

    + edit and updated story

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const session = require('express-session');
@@ -27,19 +28,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//methode override
+app.use(
+    methodOverride(function (req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+      }
+    })
+  )
 //logging
 if (process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'));
 }
 
 //handlebars helpers
-const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs');
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs');
 
 // handlebars
 app.engine(
     '.hbs',
     exphbs({
-        helpers: { formatDate, stripTags, truncate, editIcon },
+        helpers: { formatDate, stripTags, truncate, editIcon, select },
         defaultLayout: 'main',
         extname: '.hbs'
     })
